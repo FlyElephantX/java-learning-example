b636c5c83d4e6e440e4af6cc9bb3f13beae8317c
FlyElephant <sfmkeso@163.com>
2021-02-07 01:39:36 +0800

    SpringBoot 导入和导出Excel文件

---
+++ 22-spring-boot-excel/src/main/java/com/example/excel/controller/ExcelController.java
@@ -0,0 +1,58 @@
+package com.example.excel.controller;
+
+import com.alibaba.fastjson.JSON;
+import com.example.excel.dto.BusClick;
+import com.example.excel.util.ExcelUtils;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@ResponseBody
+public class ExcelController {
+
+    @RequestMapping(value = "/exportExcel", method = RequestMethod.GET)
+    public void exportExcel(HttpServletRequest httpRequest, HttpServletResponse response)  throws IOException {
+        List<BusClick> resultList = new ArrayList<>();
+        BusClick busClick = new BusClick();
+        busClick.setCityCode("a1");
+        busClick.setClientVer("a2");
+        busClick.setDate("a3");
+        busClick.setMarkId("a4");
+        busClick.setToaluv("a5");
+        resultList.add(busClick);
+
+        busClick = new BusClick();
+        busClick.setCityCode("b1");
+        busClick.setClientVer("b2");
+        busClick.setDate("b3");
+        busClick.setMarkId("b4");
+        busClick.setToaluv("b5");
+        resultList.add(busClick);
+
+        long t1 = System.currentTimeMillis();
+        ExcelUtils.writeExcel(response, resultList, BusClick.class);
+        long t2 = System.currentTimeMillis();
+        System.out.println(String.format("write over! cost:%sms", (t2 - t1)));
+    }
+
+    @RequestMapping(value = "/readExcel", method = RequestMethod.POST)
+    public void readExcel(@RequestParam(value="uploadFile", required = false) MultipartFile file){
+        long t1 = System.currentTimeMillis();
+        List<BusClick> list = ExcelUtils.readExcel("", BusClick.class, file);
+        long t2 = System.currentTimeMillis();
+        System.out.println(String.format("read over! cost:%sms", (t2 - t1)));
+        list.forEach(
+                b -> System.out.println(JSON.toJSONString(b))
+        );
+    }
+}
